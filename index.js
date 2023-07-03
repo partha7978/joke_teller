df3984bf610f7d59fbc2116eb841c5d4ad849cd0
Partha Sarathi Muduli <66170183+partha7978@users.noreply.github.com>
2023-07-04 00:06:06 +0530

    adding loading animation

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const btn = document.querySelector('.joke-btn');
 const jokeSection = document.querySelector('.joke-section'); 
+const url = "https://hindi-jokes-api.onrender.com/jokes?api_key=def3509e088f94330e14a6fde70a";
 
 // fetch the request from https://hindi-jokes-api.onrender.com/
 async function fetchRequest() {
     // https://hindi-jokes-api.onrender.com/
-
-    const get = await fetch("https://hindi-jokes-api.onrender.com/jokes?api_key=def3509e088f94330e14a6fde70a");
+    loading(true);
+    const get = await fetch(url);
     const joke = await get.json();
+    loading(false);
     return joke;
 }
 
@@ -18,3 +20,10 @@ btn.addEventListener('click', async () => {
 });
 
 btn.click();
+
+function loading(load) {
+    load === true ?
+    jokeSection.innerHTML = `<img src='./loading.gif' alt='loading image' />`
+    :
+    jokeSection.innerHTML = ``;
+}
